9b003155e44bdc6fab6e9ac05c9fb9a7fbe8ea66
Kevin Robert Stravers <macocio@gmail.com>
2018-12-16 06:57:15 +0100

    logger: Add complete documentation

--- logger.rs
+++ logger.rs
@@ -1,4 +1,34 @@
-//! An asynchronous logger
+//! A simple log-level based logger
+//!
+//! ```
+//! use universe::libs::logger::Logger;
+//!
+//! // You need to define your own message type
+//! enum MyMessageEnum {
+//!     SimpleMessage(&'static str)
+//! }
+//!
+//! // It needs to implement std::fmt::Display
+//! impl std::fmt::Display for MyMessageEnum {
+//!     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//!         match self {
+//!             MyMessageEnum::SimpleMessage(string) => write![f, "{}", string],
+//!         }
+//!     }
+//! }
+//!
+//! fn main() {
+//!     // Setup
+//!     let (mut logger, thread) = Logger::spawn();
+//!
+//!     // Actual logging
+//!     logger.info(MyMessageEnum::SimpleMessage("Hello world!"));
+//!
+//!     // Teardown
+//!     std::mem::drop(logger);
+//!     thread.join().unwrap();
+//! }
+//! ```
 use chrono::prelude::*;
 use std::{
     fmt::Display,
@@ -30,9 +60,12 @@ impl<C: 'static + Display + Send> LoggerV2Async<C> {
     /// Create a logger object and spawn a logging thread
     ///
     /// The logger object is the interface to write stuff to
-    /// the logger. The logger thread is in the background,
+    /// the logger thread. The logger thread is in the background,
     /// waiting for messages to print out. Once all logger objects
     /// are dropped, the thread will die.
+    ///
+    /// Typically, you only call `spawn` once in an application
+    /// since you just want a single logging thread to print stuff.
     pub fn spawn() -> (Logger<C>, thread::JoinHandle<()>) {
         let (tx, rx) = mpsc::sync_channel(30_000);
         let full_count = Arc::new(AtomicUsize::new(0));
@@ -48,10 +81,23 @@ impl<C: 'static + Display + Send> LoggerV2Async<C> {
         )
     }
 
+    /// The log-level is an 8-bit variable that is shared among
+    /// all clones of this logger. When we try logging we first
+    /// check if our message has a priority higher or equal to
+    /// this level. If it doesn't we just exit the logger function.
+    ///
+    /// Internally, we use an atomic variable to store the logging
+    /// level so all threads can check it quickly. This makes
+    /// log statements that won't trigger because of the log-level
+    /// absolutely fucking NUTS (so cheap it's basically
+    /// non-existent).
     pub fn set_log_level(&mut self, level: u8) {
         self.level.store(level as usize, Ordering::Relaxed);
     }
 
+    /// Generic logging function
+    ///
+    /// Send a message with a specific log-level.
     pub fn log(&mut self, level: u8, message: C) -> bool {
         if level as usize <= self.level.load(Ordering::Relaxed) {
             match self.log_channel.try_send((level, message)) {
@@ -67,28 +113,34 @@ impl<C: 'static + Display + Send> LoggerV2Async<C> {
         }
     }
 
+    /// Log an error message (level 255)
     #[cfg(not(debug_assertions))]
     pub fn trace(&mut self, message: C) -> bool {
         false
     }
 
+    /// Log an error message (level 255)
     #[cfg(debug_assertions)]
     pub fn trace(&mut self, message: C) -> bool {
         self.log(255, message)
     }
 
+    /// Log an error message (level 192)
     pub fn debug(&mut self, message: C) -> bool {
         self.log(192, message)
     }
 
+    /// Log an error message (level 128)
     pub fn info(&mut self, message: C) -> bool {
         self.log(128, message)
     }
 
+    /// Log an error message (level 64)
     pub fn warn(&mut self, message: C) -> bool {
         self.log(64, message)
     }
 
+    /// Log an error message (level 0)
     pub fn error(&mut self, message: C) -> bool {
         self.log(0, message)
     }
@@ -239,9 +291,7 @@ mod tests {
     #[bench]
     fn sending_a_complex_message_trace(b: &mut Bencher) {
         let (mut logger, thread) = Logger::<Log>::spawn();
-        b.iter(|| {
-            black_box(logger.trace(black_box(Log::Complex("Message", 3.14, &[1, 2, 3]))))
-        });
+        b.iter(|| black_box(logger.trace(black_box(Log::Complex("Message", 3.14, &[1, 2, 3])))));
         std::mem::drop(logger);
         thread.join();
     }
